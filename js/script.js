e54e72ca74c2a8e18f31d873afa504fbe815a769
zassyne <zassyne@gmail.com>
2017-07-17 11:06:55 +0300

    practice and practice..

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 
+function Person (name, age) {
+	
+	this.name = name;
+	this.age = age;
+	
 
+};
 
 
 (function main() {
@@ -12,21 +18,43 @@
 		alert("Hi " + name);
 	}
 	
-	sayHi();
+	
+	var someone = new Person("Frank", 40);
+	
+	// Trying to add a property to an object
+	try {
+		addPropertyTo(someone);
+		console.log(someone.addedProperty);
+	}
+	catch (e) {
+		console.log(e);
+	}
+	
 	
 })();
 
 
 
+// Add an arbitrary propriety to an object 
+function addPropertyTo(object) {
+	
+	if(typeof object != "object") {
+		throw new TypeError();
+	}
+	
+	object.addedProperty = "This property was added to the object!";
+	
+}
 
 
-// Capitalize the first letter //
+
+// Capitalize the first letter
 function capitalize(name) {
 	return name[0].toUpperCase() + name.substr(1, name.length - 1);
 }
 
 
-// Get current date //
+// Get current date
 function getTheDateToday() {
 	return new Date().toDateString();
 }
