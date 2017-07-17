3842caa144b95bc43d5176e017cdf867e555ba99
zassyne <zassyne@gmail.com>
2017-07-17 10:43:48 +0300

    practicing js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
 
 
 
+
+(function main() {
+
+	name = "mike";
+	name = capitalize(name);
+	
+	function sayHi() {
+		
+		alert("Hi " + name);
+	}
+	
+	sayHi();
+	
+})();
+
+
+
+
+
+// Capitalize the first letter //
+function capitalize(name) {
+	return name[0].toUpperCase() + name.substr(1, name.length - 1);
+}
+
+
+// Get current date //
+function getTheDateToday() {
+	return new Date().toDateString();
+}
+
