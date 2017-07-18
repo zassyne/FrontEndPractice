acaf89019cadc4e9b51670acdad9e2af9b9ad24f
zassyne <zassyne@gmail.com>
2017-07-18 09:46:11 +0300

    add a simple faq miniproject

---
+++ miniprojects/faq/js/faq.js
@@ -0,0 +1,35 @@
+
+
+
+window.onload = function () {
+	
+	// Get the answers elements and hide all of them.
+	var answersElements = document.getElementsByClassName("answer");
+	for(var i = 0; i<answersElements.length; i++) {
+		answersElements[i].style.display = 'none';
+	}
+	
+	// Get the questions "so that we can locate what answer to show/hide" and attach to it a "listener".
+	var questionsElements = document.getElementsByClassName("question");
+	for(var i = 0; i<questionsElements.length; i++) {
+		
+		questionsElements[i].onclick = function () { 
+			showAnswer(this);
+		}
+	}
+	
+};
+	
+
+// Function that implements the show/hide process.
+function showAnswer(question) {
+	
+	var display = question.nextElementSibling.style.display;
+	
+	if(display == 'none') {
+		question.nextElementSibling.style.display = 'block';
+	}
+	else {
+		question.nextElementSibling.style.display = 'none';
+	}
+}
