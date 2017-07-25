a9923401cfb47df47db5d336c47715a5e35b5582
zassyne <zassyne@gmail.com>
2017-07-25 09:07:25 +0300

    refactoring some code

---
+++ miniprojects/gallery/gallery/gallery.component.js
@@ -0,0 +1,56 @@
+
+gallery.component('gallery', {
+	
+	template: '<section class="row">'+
+
+				'<div class="col-sm-3" ng-repeat="photo in $ctrl.photos">'+
+					'<img src={{photo.src}} />'+
+				'</div>'+
+
+
+			'</section>',
+			
+	controller: function GalleryController() {
+	
+
+		this.photos = [
+			
+			{
+				src: "images/morocco1.jpg"
+			},
+			
+			{
+				src: "images/morocco2.jpg"
+
+			},
+			
+			{
+				src: "images/morocco3.jpg"
+
+			},
+			{
+				src: "images/morocco4.jpg"
+			},
+			
+			{
+				src: "images/morocco5.jpg"
+
+			},
+			
+			{
+				src: "images/morocco6.jpg"
+
+			},
+			{
+				src: "images/morocco7.jpg"
+			},
+			
+			{
+				src: "images/morocco8.jpg"
+
+			}
+			
+		];
+	}
+	
+});
