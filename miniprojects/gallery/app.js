f8ee68c8f2b564d352fdf42106eb757fae2a52af
zassyne <zassyne@gmail.com>
2017-07-25 07:53:14 +0300

    using components

--- miniprojects/gallery/app.js
+++ miniprojects/gallery/app.js
@@ -3,25 +3,58 @@
 var galleryApp = angular.module('galleryApp', []);
 
 
-galleryApp.controller('GalleryController', function GalleryController($scope) {
+galleryApp.component('gallery', {
 	
-	$scope.photos = [
-		
-		{
-			src: "images/morocco1.jpg"
-		},
-		
-		{
-			src: "images/morocco2.jpg"
+	template: '<section class="row">'+
 
-		},
-		
-		{
-			src: "images/morocco3.jpg"
+				'<div class="col-xs-6 col-sm-3" ng-repeat="photo in $ctrl.photos">'+
+					'<img src={{photo.src}} />'+
+				'</div>'+
 
-		}
+
+			'</section>',
+			
+	controller: function GalleryController() {
 	
-	
-	];
+
+		this.photos = [
+			
+			{
+				src: "images/morocco1.jpg"
+			},
+			
+			{
+				src: "images/morocco2.jpg"
+
+			},
+			
+			{
+				src: "images/morocco3.jpg"
+
+			},
+			{
+				src: "images/morocco4.jpg"
+			},
+			
+			{
+				src: "images/morocco5.jpg"
+
+			},
+			
+			{
+				src: "images/morocco6.jpg"
+
+			},
+			{
+				src: "images/morocco7.jpg"
+			},
+			
+			{
+				src: "images/morocco8.jpg"
+
+			}
+			
+		];
+	}
 	
 });
